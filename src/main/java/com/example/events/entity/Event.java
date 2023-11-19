22cc4e6026f07eea363d75b6d811cb7c39e276f2
deaosmanaj3 <151467375+deaosmanaj3@users.noreply.github.com>
2023-11-20 00:08:41 +0100

    Update Event.java

--- src/main/java/com/example/events/entity/Event.java
+++ src/main/java/com/example/events/entity/Event.java
@@ -1,4 +1,81 @@
 package com.example.events.entity;
 
-public class Event {
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "events")
+public class Event{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String eventName;
+    private String eventType;
+    private int seats;
+    private String performer;
+    private LocalDateTime dateAndTime;
+
+    public Event(long id, String eventName, String eventType, int seats, String performer, LocalDateTime dateAndTime) {
+        this.id = id;
+        this.eventName = eventName;
+        this.eventType = eventType;
+        this.seats = seats;
+        this.performer = performer;
+        this.dateAndTime = dateAndTime;
+    }
+
+    public LocalDateTime getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(LocalDateTime dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public String getPerformer() {
+        return performer;
+    }
+
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
 }
+
