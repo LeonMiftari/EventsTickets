7f5372929072db0b79b5aecc07be8ec78c73f871
deaosmanaj3 <151467375+deaosmanaj3@users.noreply.github.com>
2023-11-25 20:38:00 +0100

    Update UserRepository.java

--- src/main/java/com/example/events/repository/UserRepository.java
+++ src/main/java/com/example/events/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.events.repository;
 
-public class UserRepository {
+import com.example.events.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface UserRepository extends  JpaRepository<User, Long>  {
 }
