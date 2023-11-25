1c72962cdd7b7074a622de8ff507e7aed55c60af
Leon Miftari <148079413+LeonMiftari@users.noreply.github.com>
2023-11-25 14:19:39 +0100

    Adding Ticket entity

--- src/main/java/com/example/events/entity/Ticket.java
+++ src/main/java/com/example/events/entity/Ticket.java
@@ -1,4 +1,28 @@
 package com.example.events.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tickets")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String eventName;
+    private String eventLocation;
+    private LocalDateTime eventDateTime;
+    private String ticketCategory;
+    private double ticketPrice;
+    private boolean available = true;
+    private LocalDateTime createdAt;
+    private String createdBy;
 }
