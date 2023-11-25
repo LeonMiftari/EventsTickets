d6db9c1abd427949b12025602cfb0ec6ef806e1c
Dea Osmanaj <Dea.Osmanaj@student.cacttus.education>
2023-11-25 14:11:32 +0100

    Deleted getters and setters in Event file

--- src/main/java/com/example/events/entity/Event.java
+++ src/main/java/com/example/events/entity/Event.java
@@ -1,26 +1,37 @@
 package com.example.events.entity;
 
-import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "events")
-public class Event{
+public class Event {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private long id;
+
+    @Column(nullable = false, unique = true)
     private String eventName;
+
+    @Column(nullable = false)
     private String eventType;
+
+    @Column(nullable = false)
     private int seats;
+
     private String performer;
+
+    @Column(nullable = false)
     private LocalDateTime dateAndTime;
 
+    // Add validation if needed
     public Event(long id, String eventName, String eventType, int seats, String performer, LocalDateTime dateAndTime) {
         this.id = id;
         this.eventName = eventName;
@@ -29,53 +40,4 @@ public class Event{
         this.performer = performer;
         this.dateAndTime = dateAndTime;
     }
-
-    public LocalDateTime getDateAndTime() {
-        return dateAndTime;
-    }
-
-    public void setDateAndTime(LocalDateTime dateAndTime) {
-        this.dateAndTime = dateAndTime;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getEventName() {
-        return eventName;
-    }
-
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public String getEventType() {
-        return eventType;
-    }
-
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
-    public int getSeats() {
-        return seats;
-    }
-
-    public void setSeats(int seats) {
-        this.seats = seats;
-    }
-
-    public String getPerformer() {
-        return performer;
-    }
-
-    public void setPerformer(String performer) {
-        this.performer = performer;
-    }
 }
-
